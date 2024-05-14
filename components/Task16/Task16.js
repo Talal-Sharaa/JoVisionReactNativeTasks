9f2987319b203727b34331a5fd49b6a4515be549
talal-sharaa <talal.i.sharaa@gmail.com>
2024-05-14 12:47:02 +0300

    Task17: changed the button title to dynamic text

--- components/Task16/Task16.js
+++ components/Task16/Task16.js
@@ -3,10 +3,17 @@ import {Button, Text, View} from 'react-native';
 
 const Task16 = () => {
   const [showText, setShowText] = useState(false); // More descriptive variable name
+  const [titleText, setTitle] = useState('Show Text'); // More descriptive variable name
 
   return (
     <View>
-      <Button title="Toggle Text" onPress={() => setShowText(!showText)} />
+      <Button
+        title={titleText}
+        onPress={() => {
+          setShowText(!showText);
+          setTitle(showText ? 'Show Text' : 'Hide Text');
+        }}
+      />
       {showText && <Text>Talal Sharaa</Text>}
     </View>
   );
