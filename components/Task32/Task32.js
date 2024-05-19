8a0526af177ed6ee95e0c412f22afe55c43d466d
Talal-sharaa <talal.i.sharaa@gmail.com>
2024-05-19 23:00:40 +0300

    refactor: Improve image loading and scrolling in Task32 component

--- components/Task32/Task32.js
+++ components/Task32/Task32.js
@@ -1,18 +1,69 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import VideoPlayer from 'react-native-video-controls';
+import {
+  View,
+  useWindowDimensions,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import Video from 'react-native-video';
+import {SvgUri} from 'react-native-svg';
 
-const Task32 = props => {
-  // props parameter added here
+const Task32 = () => {
+  const {width, height} = useWindowDimensions();
+  const [isPaused, setIsPaused] = React.useState(false);
+
   return (
-    <View>
-      <VideoPlayer
-        source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
-        navigator={props.navigator} // changed this.props.navigator to props.navigator
-        paused={false}
-      />
+    <View style={{...styles.container, width: width, height: height}}>
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={() => {
+          setIsPaused(!isPaused);
+        }}>
+        <Video
+          source={{uri: 'https://www.w3schools.com/html/mov_bbb.mp4'}}
+          style={styles.backgroundVideo}
+          paused={isPaused}
+          repeat={true}
+        />
+      </TouchableOpacity>
+      {isPaused && (
+        <View style={styles.overlay}>
+          <TouchableOpacity
+            onPress={() => {
+              setIsPaused(!isPaused);
+            }}>
+            <SvgUri
+              uri="https://placehold.co/400x215/orange/white?text=Play"
+              style={styles.playButton}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  touchable: {
+    flex: 1,
+  },
+  backgroundVideo: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  playButton: {
+    width: '30%',
+    height: '30%',
+    resizeMode: 'contain',
+  },
+});
 
 export default Task32;
